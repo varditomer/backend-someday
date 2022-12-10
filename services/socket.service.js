bd0d277e2e025bf7b135ee090d72a7fbef68080b
Refael Abramov <refaelavramov@gmail.com>
2022-12-10 22:47:32 +0200

    sockets

--- services/socket.service.js
+++ services/socket.service.js
@@ -82,7 +82,7 @@ async function emitToUser({ type, data, userId }) {
 
 // If possible, send to all sockets BUT not the current socket 
 // Optionally, broadcast to a room / to all
-async function broadcast({ type, data, userId }) {
+async function broadcast({ type, data, room = null, userId }) {
     userId = userId.toString()
     logger.info(`Broadcasting event: ${type}`)
     const excludedSocket = await _getUserSocket(userId)
